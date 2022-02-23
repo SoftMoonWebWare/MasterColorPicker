39dc11d69da8af84b0ed3c25192525d7e5faf2ba
SoftMoon-WebWare <github.account@softmoon-webware.com>
2022-02-22 20:43:03 -0800

    updated for version 2.2 alpha

--- MasterColorPicker_desktop.php
+++ MasterColorPicker_desktop.php
@@ -5,14 +5,15 @@
 <meta name='author' content='Joe Golembieski, SoftMoon-WebWare' />
 <meta name='copyright' content='Copyright © 2013,2014,2015,2019,2020,2022 Joe Golembieski, SoftMoon-WebWare' />
 <title>MasterColorPicker desktop from SoftMoon WebWare</title>
-<link rel='stylesheet' id='MasterColorPicker_stylesheet' type='text/css' media="screen, projection" href='color-pickers/SoftMoon-WebWare/MasterColorPicker2.css' />
+<link rel='stylesheet' id='MasterColorPicker_stylesheet' type='text/css' media="screen, projection"
+			href='color-pickers/SoftMoon-WebWare/MasterColorPicker2.css' />
 <style type='text/css'>
 body, section, div {
 	margin: 0;
 	padding: 0; }
 body {
 	color: white;
-	background: black;
+	background: #808080;
   position: absolute; /* This keeps the very small body sized to the full window.  We need this for the panel-drag 'mouseover' which is on the body.  */
 	width: 100%;
 	height: 100%;  }
@@ -33,7 +34,20 @@ p img {
 	display: block; }
 </style>
 <!--  for MasterColorPicker 2  Copyright © 2012, 2013, 2018, 2019, 2020, 2022 Joe Golembieski, SoftMoon-WebWare
-      release 2.0.17  April 24, 2020   -->
+      release 2.2  Feb 22, 2022   -->
+
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/UniDOM-2020.js'></script><!--  !! ESSENTIAL !!  -->
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/input_type=numeric_.js'></script><!--  supports RainbowMaestro & ColorSpaceLab  -->
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/HTTP.js'></script><!--  ESSENTIAL for http:// (server) version, NOT for file:// use
+<!--  script type='text/javascript' src='JS_toolbucket/Log.js'></script><!--  only if you plan on logging to debug  -->
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/Picker.js'></script><!--  !! ESSENTIAL !!  -->
+<script type='text/javascript' src='JS_toolbucket/+++JS/+++.js'></script><!--  !! ESSENTIAL !!  -->
+<script type='text/javascript' src='JS_toolbucket/+++JS/+++Math.js'></script><!--  !! ESSENTIAL !!  -->
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/RGB_Calc.js'></script><!--  !! ESSENTIAL !!  -->
+<script type="text/javascript" src="JS_toolbucket/SoftMoon-WebWare/Rigden_websafe_colorblindTable_interpolator.js"></script><!--  supports RainbowMaestro & MyPalette  -->
+<script type="text/javascript" src="JS_toolbucket/skratchdot.Wickline_colorblind_converter.js"></script><!--  supports RainbowMaestro & MyPalette  -->
+<script type='text/javascript' src='JS_toolbucket/SoftMoon-WebWare/FormFieldGenie.js'></script><!--  supports MyPalette & ColorFilter  -->
+<script type="text/javascript" src="color-pickers/SoftMoon-WebWare/MasterColorPicker2.js"></script>
 
 <script type="text/javascript">
 window.addEventListener('mastercolorpicker_ready', function()  {
@@ -55,7 +69,7 @@ window.addEventListener('mastercolorpicker_ready', function()  {
 <img src='images/SoftMoonWebWare.gif' alt='SoftMoon WebWare' />
 </p>
 
-<?php include "color-pickers/SoftMoon-WebWare/MasterColorPicker2.php"; ?>
+<?php include "color-pickers/SoftMoon-WebWare/MasterColorPicker2.htm"; ?>
 
 </body>
 </html>
